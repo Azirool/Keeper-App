ae8c922456c7cb2ef7ef4b0f7d56e45c748e7f97
Azirool Fareeed <aziroolfareed31@gmail.com>
2023-09-29 18:05:06 +0800

    Implement map and props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,21 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes";
+
+
+function newNote(note){
+    return <Note
+        key={note.key}
+        title={note.title}
+        content={note.content}
+    />
+}
 
 function App(){
     return <div>
         <Header />
-        <Note />
+        {notes.map(newNote)}        
         <Footer />
     </div>;
 }
